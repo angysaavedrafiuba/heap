48eb1a675dbe43676484b9ebef87b98af191a0f6
angysaavedrafiuba <asaavedra@fi.uba.ar>
2020-07-10 20:48:13 -0300

    Se agrega heap crear

--- heap.c
+++ heap.c
@@ -1,5 +1,9 @@
 #include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include "heap.h"
+
+#define TAM 7
 
 struct heap{
 	void** datos;
@@ -7,3 +11,17 @@ struct heap{
 	size_t cantidad;
 	cmp_func_t cmp;
 };
+
+heap_t *heap_crear(cmp_func_t cmp){
+	heap_t* heap = malloc(sizeof(heap_t));
+	if(!heap) return NULL;
+
+	heap->datos = malloc(sizeof(void*)*TAM);
+	if(!heap->datos) return NULL;
+
+	heap->capacidad = TAM;
+	heap->cantidad = 0;
+	heap->cmp = cmp;
+	return heap;
+}
+
