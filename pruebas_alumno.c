b61dde7b0ca177af83fefcf3e37da4e8668c7036
angysaavedrafiuba <asaavedra@fi.uba.ar>
2020-07-12 20:55:57 -0300

    Se agregan pruebas

--- pruebas_alumno.c
+++ pruebas_alumno.c
@@ -5,8 +5,6 @@
 #include <stdio.h>
 
 
-
-// funcion de comparacion auxiliar
 int cmp_enteros(const void *a, const void *b) {
     int *x = (int*) a;
     int *y = (int*) b;
@@ -15,8 +13,6 @@ int cmp_enteros(const void *a, const void *b) {
         return 0;
     return (*x > *y) ? 1 : -1;
 }
-//cmp_func_t cmp = cmp_enteros;
-
 
 void heap_prueba_crear() {
     printf("------inicio prueba heap crear\n");
@@ -50,32 +46,70 @@ void heap_prueba_crear_arr(){
     print_test("se destruyó el heap", true);
 }
 
-/*void heap_prueba_destruir_NULL(){
+void heap_prueba_destruir_NULL(){
     printf("------inicio prueba heap destruir NULL\n");
 
-    int a[] = {6,4,7,9,1,2,8,3,5,0};
-    void* arr[10];
-
-    for(size_t i = 0; i<10; i++)
-        arr[i] = &a[i];
-    
+    int a = 1;
     heap_t *heap = heap_crear(cmp_enteros);
 
     print_test("se creó un heap", heap != NULL);
     print_test("el heap esta inicialmente vacio", heap_esta_vacio(heap));
     print_test("la cantidad en el heap es cero", heap_cantidad(heap) == 0);
+    print_test("Se inserta un elemento al heap", heap_encolar(heap, &a));
+    print_test("la cantidad en el heap es uno", heap_cantidad(heap) == 1);
+    heap_destruir(heap, NULL);
+    print_test("Se destruyo el heap con NULL", true);
+}
 
-}*/
 
-/*
 void heap_prueba_destruir_free(){
     printf("------inicio prueba heap destruir FREE\n");
-}*/
 
+    int *h = malloc(sizeof(int));
+    *h=1;
+    heap_t *heap = heap_crear(cmp_enteros);
+
+    print_test("se creó un heap", heap != NULL);
+    print_test("el heap esta inicialmente vacio", heap_esta_vacio(heap));
+    print_test("la cantidad en el heap es cero", heap_cantidad(heap) == 0);
+    print_test("Se inserta un elemento al heap", heap_encolar(heap, h));
+    print_test("la cantidad en el heap es uno", heap_cantidad(heap) == 1);
+    heap_destruir(heap, free);
+    print_test("Se destruyo el heap con NULL", true);
+}
+
+void heap_prueba_cantidad(){
+    printf("------inicio prueba heap cantidad\n");
+    int a[] = {6,4,7,9,1,2,8,3,5,0};
+    void* arr[10];
+
+    for(size_t i = 0; i<10; i++)
+        arr[i] = &a[i];
+
+    heap_t* heap = heap_crear_arr(arr, 10, cmp_enteros);
+
+    print_test("se creó un heap", heap != NULL);
+    print_test("la cantidad en el heap es diez", heap_cantidad(heap) == 10);
+    print_test("El heap desencola", heap_desencolar(heap));
+    print_test("la cantidad en heap es nueve", heap_cantidad(heap) == 9);
+    print_test("el heap vuelve a encolar", heap_encolar(heap, &a[7]));
+    print_test("la cantidad en el heap es diez", heap_cantidad(heap) == 10);
+
+    while(!heap_esta_vacio(heap))
+        heap_desencolar(heap);
+
+    print_test("El heap desencola todos los elementos", !heap_cantidad(heap));
+    print_test("El heap está vacío", heap_esta_vacio(heap));
+
+    heap_destruir(heap, NULL);
+    print_test("se destruyó el heap", true);
+}
 
 void pruebas_heap_alumno(void){
     heap_prueba_crear();
     heap_prueba_crear_arr();
-    //heap_prueba_destruir_NULL();
-    //heap_prueba_destruir_free();
+    heap_prueba_destruir_NULL();
+    heap_prueba_destruir_free();
+    heap_prueba_cantidad();
+
 }
